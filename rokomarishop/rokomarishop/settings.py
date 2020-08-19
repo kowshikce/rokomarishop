23bf173be5de351e0dd40f49bfa9b229c4f7cc39
kowshik_jr <kowshik.lm14@gmail.com>
2020-08-19 17:16:58 +0600

    secure

--- rokomarishop/rokomarishop/settings.py
+++ rokomarishop/rokomarishop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f9+oi^tg=4c)2odk+)l0-z@q-*lhi$54kvmo9wg@*pcwx60u43'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
